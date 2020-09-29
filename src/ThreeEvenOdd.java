c74e218721d996763d4499f6c2913205db8369a3
mhmmtkkl <makoklu32@gmail.com>
2020-09-29 11:39:01 -0400

    pr3

---
+++ src/ThreeEvenOdd.java
@@ -0,0 +1,22 @@
+public class ThreeEvenOdd {
+
+    /*
+
+    Given an int array as a parameter
+    return true if the array contains either 3 even or 3 odd values .
+
+    for Example:
+
+    intArray([2, 1, 3, 5]) result should be true
+    intArray([2, 1, 2, 5]) result should be  false
+    intArray([2, 4, 2, 5]) result should be  true
+
+     */
+
+    public boolean threeEvenOrOdd(int[] intArr){
+
+
+        return false;
+    }
+
+}
---
+++ src/getPopulation.java
@@ -0,0 +1,37 @@
+public class getPopulation {
+
+    /*
+        Method name is populationOfCountry
+
+        This method will get 1 String array(countryNames) 1 int array (countryPopulations) and 1 String (str)
+
+        countryNames is Country names
+
+        countryPopulations is Country population
+
+        if str is contained in the countryNames return the population
+
+
+        For example:
+
+        countryNames = {"USA" ,"Mexico" ,"Canada"}
+
+        countryPopulations = {100000 , 120000 , 130000}
+
+        str = "Mexico"
+
+        return should be 120000
+
+        NOTE : if str is not in the countryNames array return -1
+
+     */
+
+    public int populationOfCountry(String[] countryNames , int[] countryPopulations , String str){
+
+
+        return 0;
+    }
+
+
+
+}
